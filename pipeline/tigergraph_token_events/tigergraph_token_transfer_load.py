aa648b466cdc67d8fd4c6bc8be4c0b1f160ef066
yuezhang111 <yue.zhang@meshee.xyz>
2023-02-28 16:38:06 +0800

    token_transfer etl

--- pipeline/tigergraph_token_events/tigergraph_token_transfer_load.py
+++ pipeline/tigergraph_token_events/tigergraph_token_transfer_load.py
@@ -1,4 +1,94 @@
-from utils.tigergraph_connector import Tigergraph
+from utils.tigergraph_etl import DorisToTigergraphExtractTask
+from utils.tigergraph_etl import DorisToTigergraphLoadTask
 
-class TigergraphTokenTransfer(object):
-    def 
+
+class TokenTransferExtract(DorisToTigergraphExtractTask):
+    def __init__(self,bucket_size=10000):
+        super(TokenTransferExtract,self).__init__(bucket_size)
+
+    def doris_extract_sql(self,start_block):
+        sql = """
+    SELECT concat(block_number,'_',id) as transfer_id
+        ,transaction_hash as txn_hash
+        ,block_number
+        ,id as log_index
+        ,from_address
+        ,to_address
+        ,token_address
+        ,amount
+        ,transfer_time
+        ,IFNULL(symbol,'') as symbol
+    FROM dw.dwb_token_transfer_detail_eth_hi
+    WHERE block_number >= {start_block}
+    AND block_number < {end_block}
+    AND source = 'transfer'
+    AND token_address not in (
+        '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
+        '0xdac17f958d2ee523a2206206994597c13d831ec7',
+        '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+        '0x6b175474e89094c44da98b954eedeac495271d0f'
+    )
+        """.format(
+            start_block=start_block,
+            end_block=start_block+self.bucket_size
+        )
+        return sql
+
+
+def token_transfer_extract(start_block,bucket_size=1000):
+    token_transfer = TokenTransferExtract(bucket_size)
+    res_data = token_transfer.extract_doris_data(start_block)
+    return res_data
+
+
+def token_transfer_tigergraph_load(res_data):
+    vertex_token_transfer_load = DorisToTigergraphLoadTask(
+        graph_name="token_events",
+        res_data=res_data,
+        vertex="token_transfer"
+    )
+    res = vertex_token_transfer_load.load_to_tigergraph()
+    print("vertex_token_transfer_load",res)
+
+    edge_send_token_load = DorisToTigergraphLoadTask(
+        graph_name="token_events",
+        res_data=res_data,
+        edge={
+            "edge_name":"send_token",
+            "from_vertex_type":"account",
+            "from_vertex_field_name":"from_address",
+            "to_vertex_type":"token_transfer",
+            "to_vertex_field_name":"transfer_id"
+        }
+    )
+    res = edge_send_token_load.load_to_tigergraph()
+    print("edge_send_token_load",res)
+
+    edge_receive_token_load = DorisToTigergraphLoadTask(
+        graph_name="token_events",
+        res_data=res_data,
+        edge={
+            "edge_name": "receive_token",
+            "from_vertex_type": "token_transfer",
+            "from_vertex_field_name": "transfer_id",
+            "to_vertex_type": "account",
+            "to_vertex_field_name": "from_address"
+        }
+    )
+    res = edge_receive_token_load.load_to_tigergraph()
+    print("edge_receive_token_load", res)
+
+
+def main():
+    start_block = 16000000
+    bucket_size = 1000
+    end_block = 16010000
+    while start_block < end_block:
+        res_data = token_transfer_extract(start_block=start_block,bucket_size=bucket_size)
+        token_transfer_tigergraph_load(res_data)
+        start_block += bucket_size
+        print((start_block-16000000)/bucket_size + 1)
+
+
+if __name__ == "__main__":
+    main()
---
+++ utils/tigergraph_etl.py
@@ -0,0 +1,99 @@
+import json
+import requests
+from loguru import logger
+from collections import defaultdict
+from utils.doris_connector import DorisClient
+from utils.tigergraph_connector import TigergraphClient
+from utils.tigergraph_base import Vertex
+from utils.tigergraph_base import Edge
+
+
+def check_and_parse(result: requests.Response):
+    if result.status_code != 200:
+        logger.error("code: {}, body: {}", result.status_code, result.text)
+        raise Exception(result.status_code)
+    data = json.loads(result.text, strict=False)
+    x = data.get('error', False)
+    if x == 'false':
+        x = False
+    if x:
+        raise Exception(result.text)
+    return data
+
+
+class DorisToTigergraphExtractTask(object):
+    def __init__(self, bucket_size=10000):
+        self.bucket_size = bucket_size
+
+    def doris_extract_sql(self,start_block):
+        raise NotImplemented
+
+    def extract_doris_data(self, start_block):
+        db = DorisClient("dw")
+        sql = self.doris_extract_sql(start_block)
+        res_rows, field_names = db.read_sql(sql)
+        print(len(res_rows))
+        return [res_rows, field_names]
+
+
+class DorisToTigergraphLoadTask(object):
+    def __init__(self, graph_name,res_data,vertex=None,edge=None):
+        """
+        :param graph_name:
+        :param vertex: string - vertex_name
+        :param edge: dict - {edge_name:,from_vertex_type:,from_vertex_field_name:,to_vertex_type:,to_vertex_field_name:}
+        :param res_data:
+        """
+        self.graph_name = graph_name
+        self.vertex = vertex
+        self.edge = edge
+        self.res_rows = res_data[0]
+        self.field_names = res_data[1]
+
+    def transform_vertex_data(self):
+        """
+        all fields in res_data is vertex attribute
+        :return: formatted vertices dict to insert
+        """
+        vertices_dict = defaultdict(dict)
+        for row in self.res_rows:
+            attribute_dict = dict()
+            for j in range(1, len(row)):
+                attribute_dict[self.field_names[j]] = row[j]
+            Vertex(self.vertex,row[0],attribute_dict).parse_attributes_to(vertices_dict[row[0]])
+        return {'vertices':{self.vertex:vertices_dict}}
+
+    def transform_edge_data(self):
+        """
+        no attribute for edge in this version
+        :return: formatted edge dict to insert
+        """
+        from_vertex_type = self.edge["from_vertex_type"]
+        from_vertex_field_name = self.edge["from_vertex_field_name"]
+        to_vertex_type = self.edge["to_vertex_type"]
+        to_vertex_field_name = self.edge["to_vertex_field_name"]
+        from_vertex_index = self.field_names.index(from_vertex_field_name)
+        to_vertex_index = self.field_names.index(to_vertex_field_name)
+        edge_list = []
+        for row in self.res_rows:
+            from_vertex_id = row[from_vertex_index]
+            to_vertex_id = row[to_vertex_index]
+            edge_list.append(
+                Edge(self.edge["edge_name"],from_vertex_type,from_vertex_id,to_vertex_type,to_vertex_id,{})
+            )
+        return {"edges":Edge.to_tiger_graph_dict(edge_list)}
+
+    def load_to_tigergraph(self):
+        tg = TigergraphClient(self.graph_name)
+        if self.vertex is not None:
+            vertex_entities = self.transform_vertex_data()
+            vertex_load = json.dumps(vertex_entities,default=str)
+            v_dict = json.loads(vertex_load)
+            res = tg.insert(v_dict)
+            return res.text
+        if self.edge is not None:
+            edge_entities = self.transform_edge_data()
+            edge_load = json.dumps(edge_entities,default=str)
+            e_dict = json.loads(edge_load)
+            res = tg.insert(e_dict)
+            return res.text
