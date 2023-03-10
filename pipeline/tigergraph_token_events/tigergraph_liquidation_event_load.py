404d1996487e202c123bf39222e3b2684a140edb
yuezhang111 <yue.zhang@meshee.xyz>
2023-03-10 15:31:08 +0800

    [token_events]liquidation_event_load

--- pipeline/tigergraph_token_events/tigergraph_liquidation_event_load.py
+++ pipeline/tigergraph_token_events/tigergraph_liquidation_event_load.py
@@ -8,17 +8,17 @@ class LiquidationEventExtract(DorisToTigergraphExtractTask):
 
     def doris_extract_sql(self,start_block):
         sql = """
-        select concat(block_number,'_',repay_transfer_log_index) as repay_event_id
+        select concat(block_number,'_',log_index) as liquidation_event_id
             ,block_number
             ,log_index
             ,pool_address
             ,platform
-            ,liquidator as payer
+            ,liquidator
             ,borrower
-            ,liquidator_token_address as token_address
-            ,liquidator_token_amount as amount
-            ,concat(block_number,'_',liquidation_transfer_log_index) as liquidation_transfer_id
-            ,concat(block_number,'_',repay_transfer_log_index) as repay_transfer_id
+            ,liquidator_token_address
+            ,liquidator_token_amount
+            ,borrow_token_address
+            ,borrow_token_amount
         from price_oracle_test.ods_chain_liquidation_events_eth
         where block_number >= {start_block}
         AND block_number < {end_block}
@@ -36,52 +36,71 @@ def liquidation_event_extract(start_block,bucket_size=10000):
 
 
 def liquidation_event_tigergraph_load(res_data):
-    vertex_repay_event_load = DorisToTigergraphLoadTask(
+    vertex_liquidation_event_load = DorisToTigergraphLoadTask(
         graph_name="token_events",
         res_data=res_data,
         vertex={
-            "vertex_name":"repay_event",
+            "vertex_name":"liquidation_event",
             "vertex_attributes":
                 ["block_number","log_index","pool_address","platform",
-                 "payer","borrower","token_address","amount"]
+                 "liquidator","borrower",
+                 "liquidator_token_address","liquidator_token_amount",
+                 "borrow_token_address","borrow_token_amount"
+                 ]
         }
     )
-    res = vertex_repay_event_load.load_to_tigergraph()
-    print("vertex_repay_event_load",res)
+    res = vertex_liquidation_event_load.load_to_tigergraph()
+    print("vertex_liquidation_event_load",res)
 
+
+class IsLiquidationEdgeExtract(DorisToTigergraphExtractTask):
+    def __init__(self,bucket_size=10000):
+        super(IsLiquidationEdgeExtract,self).__init__(bucket_size)
+
+    def doris_extract_sql(self,start_block):
+        sql = """
+        select concat(block_number,'_',log_index) as liquidation_event_id
+            ,transfer_log_indexes
+            ,concat(block_number,'_',transfer_log_index.unnest) as transfer_id
+        from price_oracle_test.ods_chain_liquidation_events_eth
+        ,unnest(split(transfer_log_indexes,',')) transfer_log_index
+        WHERE block_number >= {start_block}
+        AND block_number < {end_block}
+        """.format(
+            start_block=start_block,
+            end_block=start_block+self.bucket_size
+        )
+        return sql
+
+
+def liquidation_edge_extract(start_block,bucket_size=10000):
+    liquidation_edge = IsLiquidationEdgeExtract(bucket_size)
+    res_data = liquidation_edge.extract_doris_data(start_block)
+    return res_data
+
+
+def liquidation_edge_tigergraph_load(res_data):
     edge_is_liquidation_load = DorisToTigergraphLoadTask(
         graph_name="token_events",
         res_data=res_data,
         edge={
             "edge_name": "is_liquidation",
-            "from_vertex_type": "repay_event",
-            "from_vertex_field_name": "repay_event_id",
+            "from_vertex_type": "liquidation_event",
+            "from_vertex_field_name": "liquidation_event_id",
             "to_vertex_type": "token_transfer",
-            "to_vertex_field_name": "liquidation_transfer_id"
+            "to_vertex_field_name": "transfer_id"
         }
     )
     res = edge_is_liquidation_load.load_to_tigergraph()
-    print("edge_is_repay_load", res)
-
-    edge_is_liquidation_load = DorisToTigergraphLoadTask(
-        graph_name="token_events",
-        res_data=res_data,
-        edge={
-            "edge_name": "is_liquidation",
-            "from_vertex_type": "repay_event",
-            "from_vertex_field_name": "repay_event_id",
-            "to_vertex_type": "token_transfer",
-            "to_vertex_field_name": "repay_transfer_id"
-        }
-    )
-    res = edge_is_liquidation_load.load_to_tigergraph()
-    print("edge_is_repay_load", res)
+    print("edge_is_liquidation_load", res)
 
 
 def load_liquidation_event(start_block,end_block=999999999,bucket_size=10000):
     while start_block < end_block:
         res_data = liquidation_event_extract(start_block=start_block,bucket_size=bucket_size)
         liquidation_event_tigergraph_load(res_data)
+        edge_data = liquidation_edge_extract(start_block=start_block,bucket_size=bucket_size)
+        liquidation_edge_tigergraph_load(edge_data)
         start_block += bucket_size
         print(start_block)
 
@@ -89,9 +108,8 @@ def load_liquidation_event(start_block,end_block=999999999,bucket_size=10000):
 def main():
     start_block = 11400000
     end_block = 11900000
-    load_liquidation_event(start_block, end_block,bucket_size=500000)
+    load_liquidation_event(start_block, end_block, bucket_size=500000)
 
 
 if __name__ == "__main__":
     main()
-
